a85bebfa24a668a9e67764f6b1de2012cfb1ffc9
Aly Aboelnasr <alyaboelnasr@gmail.com>
2023-07-07 17:23:01 +0300

    Add Logo

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connection = () => {
-    mongoose.connect( process.env.MONOGODB_URL , {
+    mongoose.connect( 'mongodb+srv://alyaboelnasr:Saba7o@cluster0.1t7wklz.mongodb.net/?retryWrites=true&w=majority' , {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => console.log("Connected to Mongoose Succesfully!")).catch(() => console.log("Couldn't Connect to Mongoose!"));
