0fc6783995987f2e5d677f203dea4f90718e78f7
Aly Aboelnasr <alyaboelnasr@gmail.com>
2023-07-07 17:59:32 +0300

    Css Changes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connection = () => {
-    mongoose.connect( 'mongodb+srv://alyaboelnasr:Saba7o@cluster0.1t7wklz.mongodb.net/?retryWrites=true&w=majority' , {
+    mongoose.connect( process.env.MONOGODB_URL , {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => console.log("Connected to Mongoose Succesfully!")).catch((e) => console.log("Couldn't Connect to Mongoose!" , e));
